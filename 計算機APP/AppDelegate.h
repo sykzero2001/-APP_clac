8f29abeb36631026566f80e4273f21aca07d18bc
Dante <deathzeroking@hotmail.com>
2015-09-30 16:34:02 +0800

    Initial Commit

---
+++ 計算機APP/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  計算機APP
+//
+//  Created by 鄭涵嚴 on 2015/9/30.
+//  Copyright © 2015年 鄭涵嚴. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 計算機APP/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  計算機APP
+//
+//  Created by 鄭涵嚴 on 2015/9/30.
+//  Copyright © 2015年 鄭涵嚴. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
